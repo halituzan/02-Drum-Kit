111b8e3097ccce2693a0ced8805dfda241bdaa03
Halit Uzan <halit.uzan@gmail.com>
2022-01-21 16:25:44 +0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let tambur1 = document.querySelector(".w");
-let tambur2 = document.querySelector(".a");
-let tambur3 = document.querySelector(".s");
-let tambur4 = document.querySelector(".d");
-let tambur5 = document.querySelector(".j");
-let tambur6 = document.querySelector(".k");
-let tambur7 = document.querySelector(".l");
+let tambur1 = document.querySelector(".w").style.backgroundImage = 'url("images/tom-w.png")';
+let tambur2 = document.querySelector(".a").style.backgroundImage = 'url("images/tom-a.png")';
+let tambur3 = document.querySelector(".s").style.backgroundImage = 'url("images/tom-s.png")';
+let tambur4 = document.querySelector(".d").style.backgroundImage = 'url("images/tom-d.png")';
+let tambur5 = document.querySelector(".j").style.backgroundImage = 'url("images/tom-j.png")';
+let tambur6 = document.querySelector(".k").style.backgroundImage = 'url("images/tom-k.png")';
+let tambur7 = document.querySelector(".l").style.backgroundImage = 'url("images/tom-l.png")';
 
 let allTambur = document.querySelectorAll(".drum")
 
 for (let i = 0; i < allTambur.length; i++) {
     allTambur[i].onclick = (e) => {
         let audio = new Audio(`./sounds/tom-${e.target.innerText}.mp3`);
+        allTambur[i].classList.add("pressed")
+        setTimeout(() => {
+            allTambur[i].classList.remove("pressed")
+        }, 200)
         audio.play();
     }
 }
@@ -19,8 +23,8 @@ document.addEventListener("keypress", (event) => {
     let audio = new Audio(`./sounds/tom-${event.key}.mp3`);
     audio.play();
     document.querySelector(`.${event.key}`).classList.add("pressed")
-    setTimeout(()=>{
+    setTimeout(() => {
         document.querySelector(`.${event.key}`).classList.remove("pressed")
-    },200)
+    }, 200)
 
 })
